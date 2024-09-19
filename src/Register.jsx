0761a3f37c4983884c44815cbfb1d4db3fe46e64
Yunus Emre <yunuzemre30@gmail.com>
2024-09-19 22:16:39 +0200

    register formik

--- src/Register.jsx
+++ src/Register.jsx
@@ -5,26 +5,56 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object({
     name: Yup.string().required("zorunlu alan"),
     email: Yup.string().email("geçersiz email adresi").required("doldurulması zorunlu alan"),
-    password:Yup.string().password("doldurulması zorunlu alan")
-    
+    password: Yup.string().password("doldurulması zorunlu alan")
 
 })
-
 const Register = () => {
 
-    const validate =(values) => {
+    const { handleChange, handleSubmit, errors, values } = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            password: ""
+        },
+        validationSchema,
+        onSubmit: (values) => {
+            console.log(values)
+        },
+    });
 
-        const errors = {}
 
-        if (!values.name) {
-            e
-        }
-    }
-  return (
-    <div>
-      ssss
-    </div>
-  )
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <input
+
+                type="text"
+                name="name"
+                placeholder='isim giriniz'
+                values={values.name}
+                onChange={handleChange}
+            />
+
+            <input
+                type="text"
+                name="email"
+                placeholder='email'
+                values={values.email}
+                onChange={handleChange}
+
+            />
+
+            <input
+                type="text"
+                name="password"
+                placeholder='password giriniz'
+                values={values.password}
+                onChange={handleChange}
+            />
+
+
+        </form>
+    )
 }
 
 export default Register
